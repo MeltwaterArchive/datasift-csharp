10d80a36ecd18db3d39cd5c6a1741efbc3970321
Courtney Robinson <courtney@crlog.info>
2011-08-03 16:49:14 +0100

    Fixed API demo to retry until it gets some buffered data

--- DatasiftApiDemo/ApiDemo.cs
+++ DatasiftApiDemo/ApiDemo.cs
@@ -14,7 +14,7 @@ namespace DataSiftDemo
         private ApiDemo()
         {
             //the configuration for this request
-            Config config = new Config(Config.ConfigType.API, "zcourts", "0c0ea89ac6b914f852c7111c85f85d03");
+            Config config = new Config(Config.ConfigType.API, "<USERNAME>", "<API_KEY>");
             //config.BufferSize = 32768;//32kb custom request buffer size,smaller tends to be faster, tune to preference
             //config.Timeout = 20000;//20 seconds time out, try not to set too low
             //config.AutoReconnect = true;
@@ -25,14 +25,21 @@ namespace DataSiftDemo
             //send a CSDL to be compiled and get the response
             DatasiftApiResponse response = request.Compile("interaction.content contains \"google\"");
             Console.WriteLine("Compiled and got hash :" + response.Hash);
-            Thread.Sleep(5000);//wait 5 seconds
+
             //make a new request, getting some interactions for the csdl we just compiled
             //to get all available interactions
             //response = request.Stream(response.Hash);
             string hash = response.Hash;
             //get a single interaction, using the third param to set the count
             response = request.Stream(hash, null, 1);
-            Console.WriteLine("Number of interactions returned : " + response.Stream.Capacity + "\n\n");
+            //buffering usually starts on the first request so if we didn't get anything back wait and try again
+            while (response.Stream.Count == 0)
+            {
+                Console.WriteLine("Nothing returned from the buffered stream sleeping for 5 seconds...");
+                Thread.Sleep(5000);//wait 5 seconds
+                response = request.Stream(hash, null, 1);
+            }
+            Console.WriteLine("Number of interactions returned : " + response.Stream.Count + "\n\n");
             Console.WriteLine("Stream response objects\n");
 
             foreach (Interaction i in response.Stream)
@@ -44,7 +51,7 @@ namespace DataSiftDemo
 
             //now see the costs for this stream
             response = request.Cost(hash);
-            Console.WriteLine("\nCosts : "+response.StreamCosts+"\n");
+            Console.WriteLine("\nCosts : " + response.StreamCosts + "\n");
 
         }
 
