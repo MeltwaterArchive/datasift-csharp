d7b82a14848cabe3219ae81796ced87d6ad69e16
Courtney Robinson <courtney@crlog.info>
2011-09-02 13:47:15 +0100

    split recording examples into its one file...need to comment out main in ApiDemo and un comment main in RecordingDemo to run recording examples

--- DatasiftApiDemo/ApiDemo.cs
+++ DatasiftApiDemo/ApiDemo.cs
@@ -24,134 +24,26 @@ namespace DataSiftDemo
             //make a new request, getting some interactions for the csdl we just compiled
             string hash = response.Hash;
             //get a single interaction, using the third param to set the count
-            //response = request.Stream(hash, null, 1);
-            ////buffering usually starts on the first request so if we didn't get anything back wait and try again
-            //while (response.Stream.Count == 0)
-            //{
-            //    Console.WriteLine("Nothing returned from the buffered stream sleeping for 5 seconds...");
-            //    Thread.Sleep(5000);//wait 5 seconds
-            //    response = request.Stream(hash, null, 1);
-            //}
-            //Console.WriteLine("Number of interactions returned : " + response.Stream.Count + "\n\n");
-            //Console.WriteLine("Stream response objects\n");
-
-            //foreach (Interaction i in response.Stream)
-            //{
-            //    Console.WriteLine(i.ToString());
-            //}
-
-            //Console.WriteLine("Stream response objects end\n\n");
-
-            ////now see the costs for this stream
-            //response = request.Cost(hash);
-            //Console.WriteLine("\nCosts : " + response.StreamCosts + "\n");
-
-//step 1    //recording operations need a list of parameters, some are optional others aren't
-            Dictionary<string, string> param = new Dictionary<string, string>();
-            //schedule a recording for this stream
-            //first thing to do is set the parameters the api requires
-            param.Add("hash", hash);//only requried param
-            param.Add("name", "My Awesome stream");
-            //we can set when to start and when to end
-            //param.Add("start", "<UNIX_TIMESTAMP>");
-            //param.Add("end", "<UNIX_TIMESTAMP>");
-//step 2    make the request - All reording related operations are done via .Recording method
-            response=request.Recording(RecordingOperation.Schedule, param);
-            Recording rec = response.Recording;
-            //the above returns data about the recording we just created
-            recordingPrinter(response,rec);
-
-//step 3    We've made a new recording and has its ID so if we want to we can update the recording details
-            param = new Dictionary<string, string>();
-            param.Add("hash", hash);//only requried param
-            param.Add("name", "New name for my awesome stream");//change the name of the stream
-            //we can set when to start and when to end, the time must be sensible!!!
-            //param.Add("start", "<UNIX_TIMESTAMP>");
-            //param.Add("end", "<UNIX_TIMESTAMP>");
-            //make the request to the API
-            response = request.Recording(RecordingOperation.Update, param);
-            recordingPrinter(response, response.Recording);
-//step 4    //exports, after data is recorded you may wish to export it, no point otherwise right?
-            param = new Dictionary<string, string>();//virtually the same process, pass some parameters
-            param.Add("recording_id", response.Recording.Id);//required
-            param.Add("format", "json");//required, supports json,xls or xlsx
-            param.Add("name", "exporting my awesome recording");//optional
-            //param.Add("start", "<UNIX_TIMESTAMP>");
-            //param.Add("end", "<UNIX_TIMESTAMP>");
-            response = request.Recording(RecordingOperation.Export_Start,param);
-            //API will return an Export object for the export we just started
-            exportPrinter(response.Export);
-
-//step 5    Get a list of our exports
-            //param = new Dictionary<string, string>();//virtually the same process, pass some parameters
-            //param.Add("id","<RECORDING_ID>");//optional, none of the params for this end point is required
-            //param.Add("page", "1");//optional :P you could paginate over all your exports if you had a vast amount
-            //param.Add("count", "20");//optional :( Limit the amount of exports returned
-            //the params above are shown for demonstration but aren't used in this request
-            response = request.Recording(RecordingOperation.Export);//yeah, param list can be optional
-            //lets see how many recordings we have
-            int size = response.ExportCount;
-            Console.WriteLine("We have" + size + " exports that we can iterate over");
-            List<Export> exports = response.AllExports;//get all the exports returned
-            foreach(Export ex in exports){
-                if (response.IsError)
-                {
-                    Console.WriteLine(response.Error);
-                }
-                else
-                {
-                    exportPrinter(ex);
-                }
-            }
-
-//step 6    We may want to delete a recording some time in the future
-            param = new Dictionary<string, string>();
-            param.Add("id", "<Recording_ID>");//only requried param
-            //make the API req.
-            //request.Recording(RecordingOperation.Delete, param);// won't do this in the demo need to use it later
-
-//step 7   We may also want to delete our exports
-            param = new Dictionary<string, string>();
-            param.Add("id", "<EXPORT_ID>");//only requried param
-            request.Recording(RecordingOperation.Export_Delete, param);//no content is returned!
-
-//step 8   After all these operations we must have used up some of our allowances!!! Lets get our usage
-            //param = new Dictionary<string, string>();
-            //param.Add("hash", "<STREAM_HASH>");//not required but would let the API return usage for a single stream
-            response=request.Usage();
-            DatasiftUsage usage = response.Usage;
-            Console.WriteLine("Delivered : " + usage.Delivered);
-            Console.WriteLine("Processed : " + usage.Processed);
-            Console.WriteLine("Twitter Delivered : " + usage.GetType("twitter").Delivered);
-            Console.WriteLine("Twitter Processed : " + usage.GetType("twitter").Processed);
-        }
-
-        private void exportPrinter(Export ex)
-        {
-            Console.WriteLine("Export name : " + ex.Name);
-            Console.WriteLine("Export ID : " + ex.Id);
-            Console.WriteLine("Recording ID : " + ex.RecordingId);
-            Console.WriteLine("Export start : " + ex.StartTime);
-            Console.WriteLine("Export end : " + ex.FinishTime);
-            Console.WriteLine("Export status : " + ex.Status);
-        }
-
-        private void recordingPrinter(DatasiftApiResponse response,Recording rec)
-        {
-
-            if (response.IsError)
+            response = request.Stream(hash, null, 1);
+            //buffering usually starts on the first request so if we didn't get anything back wait and try again
+            while (response.Stream.Count == 0)
             {
-                Console.WriteLine(response.Error);
+                Console.WriteLine("Nothing returned from the buffered stream sleeping for 5 seconds...");
+                Thread.Sleep(5000);//wait 5 seconds
+                response = request.Stream(hash, null, 1);
             }
-            else
+            Console.WriteLine("Number of interactions returned : " + response.Stream.Count + "\n\n");
+            Console.WriteLine("Stream response objects\n");
+
+            foreach (Interaction i in response.Stream)
             {
-                //we can not print data about it
-                Console.WriteLine("Start time : " + rec.StartTime);
-                Console.WriteLine("FInish time : " + rec.FinishTime);
-                Console.WriteLine("Name : " + rec.Name);
-                Console.WriteLine("ID : " + rec.Id);
-                Console.WriteLine("Hash : " + rec.Hash);
+                Console.WriteLine(i.ToString());
             }
+
+            Console.WriteLine("Stream response objects end\n\n");
+            //now see the costs for this stream
+            response = request.Cost(hash);
+            Console.WriteLine("\nCosts : " + response.StreamCosts + "\n");
         }
 
         static void Main(string[] args)
